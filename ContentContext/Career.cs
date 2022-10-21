0ec643b8d890fa57a2d0f7c54b50ff4f2d1b7731
Felipe Dias <adfelipealmeida@gmail.com>
2022-10-21 08:02:58 -0300

    modelando a parte de carreiras

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -2,6 +2,12 @@ namespace Objetos.ContentContext
 {
     public class Career : Content
     {
-        public int Courses { get; set; }
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+
+        public IList<CareerItem> Items { get; set; }
+        public int TotalCourses => Items.Count; // Expression Body, usa somente o get;
     }
 }
